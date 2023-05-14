807d251ac9f29c718001d46fb1e1dd539685c634
rkarumba <rkarumba@outlook.com>
2023-05-14 13:51:58 -0700

    String Interpolation

---
+++ String Interpolation.playground/Contents.swift
@@ -0,0 +1,10 @@
+// This variable 'score' stores an integer value of 85.
+var score = 85
+
+// This variable 'str' uses string interpolation to create a string that includes the value of 'score'.
+// The resulting string will be " Your score was 85".
+var str = " Your score was \(score)"
+
+// This variable 'results' uses string interpolation to create a string that includes the value of 'str'.
+// The resulting string will be " Your Results are here: Your score was 85".
+var results = " Your Results are here: \(str)"
