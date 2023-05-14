216452a5c889f4515c650fd5352f026e1b92fe44
rkarumba <rkarumba@outlook.com>
2023-05-14 12:54:16 -0700

    "Initial commit"

---
+++ Strings & Integers .playground/Contents.swift
@@ -0,0 +1,6 @@
+// This variable 'age' represents a person's age and is assigned a value of 38
+var age = 38
+
+// This variable 'population' represents the population of a location and is assigned a value of 8 million.
+// The underscore (_) is used as a visual separator to make it easier to read larger numbers. This doesnt affect the code
+var population = 8_000_000
---
+++ VariablesPlayground.playground/Contents.swift
@@ -0,0 +1,3 @@
+var str = "Hello Playground"
+
+str = "dog"
