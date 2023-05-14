02611459c353a830ef31820a16a486f8b6eb78f2
rkarumba <rkarumba@outlook.com>
2023-05-14 13:08:32 -0700

    Daily Commit

---
+++ Multi-Line Strings.playground/Contents.swift
@@ -0,0 +1,13 @@
+// This variable 'str' is a multi-line string literal that spans across three lines and contains line breaks and indentation.
+var str = """
+This goes
+over multiple
+lines
+"""
+
+// This variable 'str2' is also a multi-line string literal that spans across three lines, but it uses the backslash (\) character to escape the line breaks and indentation.
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,5 @@
+// This variable 'str' is assigned the value of "Hello Playground"
 var str = "Hello Playground"
 
+// This line of code assigns the value "dog" to the variable 'str', effectively replacing its previous value.
 str = "dog"
