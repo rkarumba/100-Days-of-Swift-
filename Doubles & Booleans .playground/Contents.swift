11d154390d2377dd64c688c65ad869e9dbf65b07
rkarumba <rkarumba@outlook.com>
2023-05-14 13:36:48 -0700

    commit

---
+++ Doubles & Booleans .playground/Contents.swift
@@ -0,0 +1,24 @@
+// This variable 'pi' is a floating-point number with a value of 3.141. It is of the Double data type.
+var pi: Double = 3.141
+
+// This variable 'awesome' is a Boolean value with a value of true. It is of the Bool data type.
+var awesome: Bool = true
+
+// Double Data Type:
+// The 'Double' data type is used to store decimal numbers with greater precision than the 'Float' data type.
+// You can perform mathematical operations on 'Double' values, like addition, subtraction, multiplication, and division.
+var doubleValue1: Double = 3.5
+var doubleValue2: Double = 4.2
+var sum = doubleValue1 + doubleValue2 // sum = 7.7
+
+// Bool Data Type:
+// The 'Bool' data type is used to store Boolean values, which can be either 'true' or 'false'.
+// You can use 'Bool' values in conditional statements to control the flow of your program.
+var isRaining: Bool = true
+if isRaining {
+    print("Bring an umbrella")
+} else {
+    print("Leave the umbrella at home")
+}
+
+
