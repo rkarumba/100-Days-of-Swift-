ed71a175725575185d7aff89d8c113f91510e112
rkarumba <rkarumba@outlook.com>
2023-05-14 13:57:33 -0700

    Commit

---
+++ Constants .playground/Contents.swift
@@ -0,0 +1,8 @@
+// This constant 'taylor' stores a string value "Swift".
+// The 'String' data type is used to store text.
+let taylor: String = "Swift"
+
+// This constant 'him' stores a string value "Luis".
+// The 'String' data type is used to store text.
+let him: String = "Luis"
+
